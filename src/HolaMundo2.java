69821c325a5dd6008b17c4bc8b3b4bc8e5677a29
DiegoGarron <diegoooblue@gmail.com>
2018-02-08 10:01:50 +0100

    Prueba para subir proyecto a repositorio

---
+++ src/HolaMundo2.java
@@ -0,0 +1,27 @@
+
+public class HolaMundo2 {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Hola Mundo 2 !!!");
+		System.out.println(3 + 4 + 5);
+		System.out.println("3 + 4 + 5");
+	
+		int x= 3;
+		int y= 4;
+		int xy= 11;
+		int cos= 5;
+		
+		double a= ((x+1 * 4) * (2+y));
+		System.out.println(a);
+		
+		double b= (x + y/2 + xy);
+		System.out.println(b);
+		
+		double c= 4 *cos /2;
+		System.out.println(c);
+		
+		
+	}
+
+}
